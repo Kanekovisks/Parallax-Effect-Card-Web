94f3a2bf27b36c92820e23333523f8a9e41d408b
Kanekovisks <gabriel.roxo2006@gmail.com>
2025-10-17 08:42:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ https://www.w3schools.com/js/default.asp - W3S School JavaScript
 https://developer.mozilla.org/en-US/docs/Web/JavaScript - MDN Web Docs JavaScript
 */
 
-// Definição de constantes
+// Definição de constantes das classes
 const container = document.querySelector('.container'); // Definição de "container"
 const card = document.querySelector('.card'); // Definição de "card"
 
 // Detector de sobreposição do mouse
 container.addEventListener('mousemove', (e) => {
-    // Definição suavidade durante sobreposição
+    // Definição de suavidade durante sobreposição
     card.style.transition = 'transform 0.1s ease';
 
-    // Dados de área
+    // Dados de área do card
     const rect = card.getBoundingClientRect();
     const x = e.clientX - rect.left - rect.width / 2; // Definição de área X
     const y = e.clientY - rect.top - rect.height / 2; // Definição de área Y
@@ -22,16 +22,17 @@ container.addEventListener('mousemove', (e) => {
     const rotateX = (y / rect.height) * 40; // Intensidade do movimento Y
     const rotateY = (x / rect.width) * 40; // Instensidade do movimento X
 
-    // Angulação do "card"
+    // Angulação de rotação do card
     card.style.transform = `rotateY(${rotateY}deg) rotateX(${-rotateX}deg)`; // Ângulo de rotação de interação com o cursor
 });
 
 
 container.addEventListener('mouseleave', () => {
-    // Definição suavidade após retirada do mouse
+    // Definição de suavidade após retirada do mouse
     card.style.transition = 'transform 0.6s ease'; 
 
-    // Redefinição do posicionamento
+    // Redefinição do posicionamento do card
     card.style.transform = 'rotateY(0) rotateX(0)';
 });
+
 
